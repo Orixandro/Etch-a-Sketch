6eccf5d6ce757fcbfcfebbbc41e599371a8300c8
Orixandro <orixandro@gmail.com>
2025-08-08 18:19:50 +0200

    Make the grid automatic and add features
    I have made it so the grid can take any size,
    apart from that I have included features like
    reseting the grid, changing the size, erasing by
    pressing shift...

--- javascript.js
+++ javascript.js
@@ -1,12 +1,47 @@
 function createGrid(container, size) {
-    for (let i = 0; i < (size * size); i++) {
-        let square = document.createElement("div")
-        square.addEventListener("mouseover", () => square.classList.add("active"))
-        container.appendChild(square)
+    for (let i = 0; i < size; i++) {
+        let subDivision = document.createElement("div")
+        for (let j = 0; j < size; j++) {
+            let square = document.createElement("div")
+            square.addEventListener("mouseover", (e) => {
+                if (e.ctrlKey) {
+                    square.classList.add("active")
+                } else if (e.shiftKey) {
+                    square.classList.remove("active")
+                }
+            })
+            subDivision.appendChild(square)
+        }
+        container.appendChild(subDivision)
     }
 }
 
+function deleteGrid(container) {
+    container.replaceChildren();
+}
+
+function resetGrid(container, size) {
+    deleteGrid(container)
+    createGrid(container, size)
+}
+
 const container = document.querySelector("#gridContainer")
-const size = 16
+let size = 120
 
 createGrid(container, size)
+
+const borderButton = document.querySelector("#borderButton")
+borderButton.addEventListener("click", () => {
+    const grid = document.querySelectorAll("#gridContainer > div > div")
+    grid.forEach((square) => square.classList.toggle("border"))
+})
+
+const resetButton = document.querySelector("#resetButton")
+resetButton.addEventListener("click", () => resetGrid(container, size))
+
+const sizeList = document.querySelector("#sizeList")
+const sizeButton = document.querySelector("#sizeButton")
+sizeButton.addEventListener("click", () => {
+    size = Number(sizeList.value)
+    resetGrid(container, size)
+})
